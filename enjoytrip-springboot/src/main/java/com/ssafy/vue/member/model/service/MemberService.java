af20252c1bc77b0663ea4371076b67882af5b5df
LeeSearch <ukyoung147@gmail.com>
2023-11-18 23:33:45 +0900

    Add_서버추가_1118

---
+++ enjoytrip-springboot/src/main/java/com/ssafy/vue/member/model/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.ssafy.vue.member.model.service;
+
+import com.ssafy.vue.member.model.MemberDto;
+
+import java.lang.reflect.Member;
+
+public interface MemberService {
+
+	MemberDto login(MemberDto memberDto) throws Exception;
+	MemberDto userInfo(String userId) throws Exception;
+	void saveRefreshToken(String userId, String refreshToken) throws Exception;
+	Object getRefreshToken(String userId) throws Exception;
+	void deleRefreshToken(String userId) throws Exception;
+	int idExistingCheck(String userId) throws Exception; // 아이디 중복 체크
+	void userRegistration(MemberDto memberDto) throws Exception;	// 회원 가입ㄴ
+}
