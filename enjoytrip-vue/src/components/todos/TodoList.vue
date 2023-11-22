42e4eb6142d9b9ef4afb7a1ffec026116e2e78bf
복영석 <dudtjr856@gmail.com>
2023-11-22 21:37:47 +0900

    이미지올리기실패 + 여행공유기능

--- enjoytrip-vue/src/components/todos/TodoList.vue
+++ enjoytrip-vue/src/components/todos/TodoList.vue
@@ -42,65 +42,86 @@
     <!-- 버튼들을 오른쪽 하단으로 이동 -->
     <div class="d-flex justify-content-end mt-3">
       <!-- 저장하기 버튼 -->
-      <button type="button" class="btn btn-primary" @click="saveData">
-        저장하기
-      </button>
+    
 
       <!-- 공유하기 버튼 -->
-      <button type="button" class="btn btn-success ml-2" @click="writeNew">
-        공유하기
+      <form @submit.prevent="onSubmit">
+    <div class="col-auto text-center">
+      <button type="submit" class="btn btn-outline-primary mb-3">
+        글작성
       </button>
+    </div>
+  </form>
     </div>
   </div>
 </template>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <script setup>
+import { ref, computed } from "vue";
 import { useTodoStore } from "@/stores/todo";
+import { useRouter } from "vue-router";
+import { registArticle } from "@/api/board";
+import { useMemberStore } from "@/stores/member";
+
+const memberStore = useMemberStore();
+const userInfo = computed(() => memberStore.userInfo);
+const router = useRouter();
 
 const todoStore = useTodoStore();
 
-// 저장하기 버튼 클릭 시 실행되는 함수
-const saveData = () => {
-  // 저장 로직을 여기에 추가
-  console.log("저장하기 버튼이 클릭되었습니다.");
-};
+const article = ref({
+  articleNo: 0,
+  subject: "", // subject 초기값은 빈 문자열로 설정
+  content: "", // content 초기값은 빈 문자열로 설정
+  userId: "",
+  userName: "",
+  hit: 0,
+  registerTime: "",
+});
 
-// 공유하기 버튼 클릭 시 실행되는 함수
-const writeNew = () => {
-  // 공유하기 로직을 여기에 추가
-  console.log("공유하기 버튼이 클릭되었습니다.");
-};
+function onSubmit() {
+  // content에 로컬 스토리지에 있는 모든 내용을 집어넣기
+  article.value.content = processTodos(todoStore.todos);
+
+  // subject에 "userName이 여행경로를 공유했습니다"와 같은 내용 참조
+  article.value.subject = `${userInfo.value.userName}이(가) 여행경로를 공유했습니다.`;
+
+  // 나머지 글 작성 로직은 그대로 유지
+  writeArticle();
+}
+
+function processTodos(todos) {
+  let result = "";
+
+  todos.forEach((todo, index) => {
+    result += `${index + 1}번: ${todo.addr1}\n`;
+  });
+
+  return result;
+}
+
+
+function writeArticle() {
+  console.log("글등록하자!!", article.value);
+  article.value.userId = userInfo.value.userId;
+  article.value.userName = userInfo.value.userName;
+  registArticle(
+    article.value,
+    (response) => {
+      let msg = "글등록 처리시 문제 발생했습니다.";
+      if (response.status == 201) msg = "글등록이 완료되었습니다.";
+      alert(msg);
+      moveList();
+    },
+    (error) => console.log(error)
+  );
+}
+
+function moveList() {
+  router.push({ name: "article-list" });
+}
 </script>
+
 <style scoped>
-/* Set a fixed size for images */
-img.img-thumbnail {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-}
-
-.no-image-placeholder img {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-}
-
-/* Set a fixed height for table rows */
-
-
-</style>
+/* 스타일 코드 생략 */
+</style> 
